4b74bf5001ae8150263308cd28c08a3d89ecb60d
Laura Salazar Higuita <laurasalazarh@gmail.com>
2026-04-24 20:39:04 -0500

    Adding base path

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: "/laura-studio",
   images: {
     unoptimized: true, // GitHub Pages no soporta optimización de imágenes
   },
